0fca0f21429dfe938bf85bba34a1bf36f6764a91
yongmaru <film.shishi123@gmail.com>
2022-08-30 20:42:31 +0700

    Commit_day1

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  
+  //General
+  static String appName = 'ChampShop';
+
+  //Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = 'buyerService';
+  static String routeSalerService = 'salerService';
+
+
+}
