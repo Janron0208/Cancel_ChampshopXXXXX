bedca176dcb172e9f59016ce12016e2dd3ba57ec
yongmaru <film.shishi123@gmail.com>
2022-08-31 20:21:13 +0700

    Commit_day2

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,3 +1,6 @@
+import 'package:champshop/utility/my_constant.dart';
+import 'package:champshop/widgets/show_image.dart';
+import 'package:champshop/widgets/show_title.dart';
 import 'package:flutter/material.dart';
 
 class Authen extends StatefulWidget {
@@ -10,10 +13,37 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Text('This is ChampShop'),
+        child: ListView(
+          children: [
+            buildImage(size),
+            buildAppName()
+          ],
+        ),
       ),
+    );
+  }
+
+  Row buildAppName() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ShowTitle(
+                  title: MyConstant.appName, textStyle: MyConstant().h1Style()),
+            ],
+          );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.45,
+          child: ShowImage(path: MyConstant.image1),
+        ),
+      ],
     );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -11,5 +11,33 @@ class MyConstant {
   static String routeBuyerService = 'buyerService';
   static String routeSalerService = 'salerService';
 
+  //Image
+  static String image1 = 'images/image1.png';
+
+  //color
+  static Color primary1 = Color.fromARGB(255, 116, 78, 148);
+  static Color primary2 = Color.fromARGB(255, 146, 117, 189);
+  static Color primary3 = Color.fromARGB(255, 192, 151, 225);
+  static Color primary4 = Color.fromARGB(255, 215, 192, 239);
+
+  //Style
+  TextStyle h1Style() => TextStyle(
+    fontSize: 24,
+    color: primary1,
+    fontWeight: FontWeight.bold,
+  );
+   TextStyle h2Style() => TextStyle(
+    fontSize: 18,
+    color: primary2,
+    fontWeight: FontWeight.w700,
+  );
+   TextStyle h3Style() => TextStyle(
+    fontSize: 14,
+    color: primary3,
+    fontWeight: FontWeight.normal,
+  );
+
+
+
 
 }
